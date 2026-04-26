91b0882b0829402d6c70714f68bbac3f1a84e695
Aganur <agpro5688@gmail.com>
2026-04-26 16:24:09 +0500

    1.4

--- database/factories/FoodFactory.php
+++ database/factories/FoodFactory.php
@@ -26,7 +26,7 @@ class FoodFactory extends Factory
             'price'=> fake()->numberBetween(10, 100),
             'code'=> fake()->numberBetween(10000, 99999),
             'like_count'=> fake()->numberBetween(0,10000),
-            // 3 commit
+            // In sonky versiya
             
         ];
     }
